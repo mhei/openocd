54cfed1a4d7bc4a2357db08627225ccc38fd64ce
Michael Heimpold <mhei@heimpold.de>
2024-03-20 21:34:31 +0100

    drivers/linuxgpiodv2: introduce new driver for libgpiod v2 API
    This driver is a copy of the existing linuxgpiod driver but adapted
    to the newer v2 API of libgpiod.
    
    I've chosen to add it in parallel to the existing driver so that it
    is possible to have releases with support for both.
    
    I decided to make a dedicated new module to prevent having a lot of
    ifdef-ery inside the C file itself - in my eyes, the small amount of
    additionl makefile magic is simpler.
    
    The version of libgpiod is detected during configure time based on
    the pkg-config version detection since the library itself does not
    provide any defines or similar stuff.
    
    Change-Id: I38bdbd404b678ed6609ce790c47234a08cf85b54
    Signed-off-by: Michael Heimpold <mhei@heimpold.de>

---
+++ src/jtag/drivers/linuxgpiodv2.c
@@ -0,0 +1,521 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Bitbang driver for Linux GPIO descriptors through libgpiod
+ * using the library's v2 API.
+ * Copyright (C) 2024 Michael Heimpold <mhei@heimpold.de>
+ *
+ * Derived from linuxgpiod.c:
+ * Copyright (C) 2020 Antonio Borneo <borneo.antonio@gmail.com>
+ *
+ * Which in turn was largely based on sysfsgpio driver:
+ * Copyright (C) 2012 by Creative Product Design, marc @ cpdesign.com.au
+ * Copyright (C) 2014 by Jean-Christian de Rivaz <jc@eclis.ch>
+ * Copyright (C) 2014 by Paul Fertser <fercerpav@gmail.com>
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gpiod.h>
+#include <jtag/adapter.h>
+#include <jtag/interface.h>
+#include <transport/transport.h>
+#include "bitbang.h"
+
+static struct gpiod_chip *gpiod_chip[ADAPTER_GPIO_IDX_NUM] = {};
+static struct gpiod_line_settings *gpiod_line_settings[ADAPTER_GPIO_IDX_NUM] = {};
+static struct gpiod_line_config *gpiod_line_config[ADAPTER_GPIO_IDX_NUM] = {};
+static struct gpiod_line_request *gpiod_line_request[ADAPTER_GPIO_IDX_NUM] = {};
+
+static int last_swclk;
+static int last_swdio;
+static bool last_stored;
+static bool swdio_input;
+
+static const struct adapter_gpio_config *adapter_gpio_config;
+
+/* Helper to get/set a single line */
+static int linuxgpiod_line_get_value(enum adapter_gpio_config_index idx)
+{
+	return gpiod_line_request_get_value(gpiod_line_request[idx],
+			adapter_gpio_config[idx].gpio_num);
+}
+
+static int linuxgpiod_line_set_value(enum adapter_gpio_config_index idx, int value)
+{
+	return gpiod_line_request_set_value(gpiod_line_request[idx],
+			adapter_gpio_config[idx].gpio_num,
+			value);
+}
+
+/*
+ * Helper function to determine if gpio config is valid
+ *
+ * Assume here that there will be less than 10000 gpios per gpiochip, and less
+ * than 1000 gpiochips.
+ */
+static bool is_gpio_config_valid(enum adapter_gpio_config_index idx)
+{
+	return adapter_gpio_config[idx].chip_num < 1000
+		&& adapter_gpio_config[idx].gpio_num < 10000;
+}
+
+/* Bitbang interface read of TDO */
+static bb_value_t linuxgpiod_read(void)
+{
+	int retval;
+
+	retval = linuxgpiod_line_get_value(ADAPTER_GPIO_IDX_TDO);
+	if (retval < 0) {
+		LOG_WARNING("reading tdo failed");
+		return 0;
+	}
+
+	return retval ? BB_HIGH : BB_LOW;
+}
+
+/*
+ * Bitbang interface write of TCK, TMS, TDI
+ *
+ * Seeing as this is the only function where the outputs are changed,
+ * we can cache the old value to avoid needlessly writing it.
+ */
+static int linuxgpiod_write(int tck, int tms, int tdi)
+{
+	static int last_tck;
+	static int last_tms;
+	static int last_tdi;
+
+	static int first_time;
+
+	int retval;
+
+	if (!first_time) {
+		last_tck = !tck;
+		last_tms = !tms;
+		last_tdi = !tdi;
+		first_time = 1;
+	}
+
+	if (tdi != last_tdi) {
+		retval = linuxgpiod_line_set_value(ADAPTER_GPIO_IDX_TDI, tdi);
+		if (retval < 0)
+			LOG_WARNING("writing tdi failed");
+	}
+
+	if (tms != last_tms) {
+		retval = linuxgpiod_line_set_value(ADAPTER_GPIO_IDX_TMS, tms);
+		if (retval < 0)
+			LOG_WARNING("writing tms failed");
+	}
+
+	/* write clk last */
+	if (tck != last_tck) {
+		retval = linuxgpiod_line_set_value(ADAPTER_GPIO_IDX_TCK, tck);
+		if (retval < 0)
+			LOG_WARNING("writing tck failed");
+	}
+
+	last_tdi = tdi;
+	last_tms = tms;
+	last_tck = tck;
+
+	return ERROR_OK;
+}
+
+static int linuxgpiod_swdio_read(void)
+{
+	int retval;
+
+	retval = linuxgpiod_line_get_value(ADAPTER_GPIO_IDX_SWDIO);
+	if (retval < 0) {
+		LOG_WARNING("Fail read swdio");
+		return 0;
+	}
+
+	return retval;
+}
+
+static void linuxgpiod_swdio_drive(bool is_output)
+{
+	int retval;
+
+	if (is_output) {
+		if (gpiod_line_request[ADAPTER_GPIO_IDX_SWDIO_DIR]) {
+			retval = linuxgpiod_line_set_value(ADAPTER_GPIO_IDX_SWDIO_DIR, 1);
+			if (retval < 0)
+				LOG_WARNING("Failed to set swdio_dir=1");
+		}
+
+		retval = gpiod_line_settings_set_direction(gpiod_line_settings[ADAPTER_GPIO_IDX_SWDIO],
+				GPIOD_LINE_DIRECTION_OUTPUT);
+		if (retval < 0)
+			LOG_WARNING("Failed to set new direction of swdio");
+
+		retval = gpiod_line_settings_set_output_value(gpiod_line_settings[ADAPTER_GPIO_IDX_SWDIO],
+				GPIOD_LINE_VALUE_ACTIVE);
+		if (retval < 0)
+			LOG_WARNING("Failed to set output value of swdio");
+
+		retval = gpiod_line_config_add_line_settings(gpiod_line_config[ADAPTER_GPIO_IDX_SWDIO],
+				&adapter_gpio_config[ADAPTER_GPIO_IDX_SWDIO].gpio_num, 1,
+				gpiod_line_settings[ADAPTER_GPIO_IDX_SWDIO]);
+		if (retval < 0)
+			LOG_WARNING("Failed to apply output configuration to swdio");
+
+		retval = gpiod_line_request_reconfigure_lines(gpiod_line_request[ADAPTER_GPIO_IDX_SWDIO],
+				gpiod_line_config[ADAPTER_GPIO_IDX_SWDIO]);
+		if (retval < 0)
+			LOG_WARNING("Failed to switch swdio to output");
+	} else {
+		retval = gpiod_line_settings_set_direction(gpiod_line_settings[ADAPTER_GPIO_IDX_SWDIO],
+				GPIOD_LINE_DIRECTION_INPUT);
+		if (retval < 0)
+			LOG_WARNING("Failed to switch swdio to output");
+
+		retval = gpiod_line_config_add_line_settings(gpiod_line_config[ADAPTER_GPIO_IDX_SWDIO],
+				&adapter_gpio_config[ADAPTER_GPIO_IDX_SWDIO].gpio_num, 1,
+				gpiod_line_settings[ADAPTER_GPIO_IDX_SWDIO]);
+		if (retval < 0)
+			LOG_WARNING("Failed to apply input configuration to swdio");
+
+		retval = gpiod_line_request_reconfigure_lines(gpiod_line_request[ADAPTER_GPIO_IDX_SWDIO],
+				gpiod_line_config[ADAPTER_GPIO_IDX_SWDIO]);
+		if (retval < 0)
+			LOG_WARNING("Failed to switch swdio to input");
+
+		if (gpiod_line_request[ADAPTER_GPIO_IDX_SWDIO_DIR]) {
+			retval = linuxgpiod_line_set_value(ADAPTER_GPIO_IDX_SWDIO_DIR, 0);
+			if (retval < 0)
+				LOG_WARNING("Failed to set swdio_dir=0");
+		}
+	}
+
+	last_stored = false;
+	swdio_input = !is_output;
+}
+
+static int linuxgpiod_swd_write(int swclk, int swdio)
+{
+	int retval;
+
+	if (!swdio_input) {
+		if (!last_stored || (swdio != last_swdio)) {
+			retval = linuxgpiod_line_set_value(ADAPTER_GPIO_IDX_SWDIO, swdio);
+			if (retval < 0)
+				LOG_WARNING("Fail set swdio");
+		}
+	}
+
+	/* write swclk last */
+	if (!last_stored || (swclk != last_swclk)) {
+		retval = linuxgpiod_line_set_value(ADAPTER_GPIO_IDX_SWCLK, swclk);
+		if (retval < 0)
+			LOG_WARNING("Fail set swclk");
+	}
+
+	last_swdio = swdio;
+	last_swclk = swclk;
+	last_stored = true;
+
+	return ERROR_OK;
+}
+
+static int linuxgpiod_blink(int on)
+{
+	int retval;
+
+	if (!is_gpio_config_valid(ADAPTER_GPIO_IDX_LED))
+		return ERROR_OK;
+
+	retval = linuxgpiod_line_set_value(ADAPTER_GPIO_IDX_LED, on);
+	if (retval < 0)
+		LOG_WARNING("Fail set led");
+	return retval;
+}
+
+static struct bitbang_interface linuxgpiod_bitbang = {
+	.read = linuxgpiod_read,
+	.write = linuxgpiod_write,
+	.swdio_read = linuxgpiod_swdio_read,
+	.swdio_drive = linuxgpiod_swdio_drive,
+	.swd_write = linuxgpiod_swd_write,
+	.blink = linuxgpiod_blink,
+};
+
+/*
+ * Bitbang interface to manipulate reset lines SRST and TRST
+ *
+ * (1) assert or (0) deassert reset lines
+ */
+static int linuxgpiod_reset(int trst, int srst)
+{
+	int retval1 = 0, retval2 = 0;
+
+	LOG_DEBUG("linuxgpiod_reset");
+
+	/*
+	 * active low behavior handled by "adaptor gpio" command and
+	 * thus handled when requesting the line.
+	 */
+	if (gpiod_line_request[ADAPTER_GPIO_IDX_SRST]) {
+		retval1 = linuxgpiod_line_set_value(ADAPTER_GPIO_IDX_SRST, srst);
+		if (retval1 < 0)
+			LOG_WARNING("set srst value failed");
+	}
+
+	if (gpiod_line_request[ADAPTER_GPIO_IDX_TRST]) {
+		retval2 = linuxgpiod_line_set_value(ADAPTER_GPIO_IDX_TRST, trst);
+		if (retval2 < 0)
+			LOG_WARNING("set trst value failed");
+	}
+
+	return ((retval1 < 0) || (retval2 < 0)) ? ERROR_FAIL : ERROR_OK;
+}
+
+static bool linuxgpiod_jtag_mode_possible(void)
+{
+	if (!is_gpio_config_valid(ADAPTER_GPIO_IDX_TCK))
+		return false;
+	if (!is_gpio_config_valid(ADAPTER_GPIO_IDX_TMS))
+		return false;
+	if (!is_gpio_config_valid(ADAPTER_GPIO_IDX_TDI))
+		return false;
+	if (!is_gpio_config_valid(ADAPTER_GPIO_IDX_TDO))
+		return false;
+	return true;
+}
+
+static bool linuxgpiod_swd_mode_possible(void)
+{
+	if (!is_gpio_config_valid(ADAPTER_GPIO_IDX_SWCLK))
+		return false;
+	if (!is_gpio_config_valid(ADAPTER_GPIO_IDX_SWDIO))
+		return false;
+	return true;
+}
+
+static inline void helper_release(enum adapter_gpio_config_index idx)
+{
+	if (gpiod_line_request[idx]) {
+		gpiod_line_request_release(gpiod_line_request[idx]);
+		gpiod_line_request[idx] = NULL;
+	}
+	if (gpiod_line_config[idx]) {
+		gpiod_line_config_free(gpiod_line_config[idx]);
+		gpiod_line_config[idx] = NULL;
+	}
+	if (gpiod_line_settings[idx]) {
+		gpiod_line_settings_free(gpiod_line_settings[idx]);
+		gpiod_line_settings[idx] = NULL;
+	}
+	if (gpiod_chip[idx]) {
+		gpiod_chip_close(gpiod_chip[idx]);
+		gpiod_chip[idx] = NULL;
+	}
+}
+
+static int linuxgpiod_quit(void)
+{
+	LOG_DEBUG("linuxgpiod_quit");
+	for (int i = 0; i < ADAPTER_GPIO_IDX_NUM; ++i)
+		helper_release(i);
+
+	return ERROR_OK;
+}
+
+static struct gpiod_chip *gpiod_chip_open_by_number(unsigned int chip_num)
+{
+	char chip_path[32];
+	int l;
+
+	l = snprintf(chip_path, sizeof(chip_path), "/dev/gpiochip%u", chip_num);
+	if (l < 0 || l >= (int)sizeof(chip_path))
+		return NULL;
+
+	return gpiod_chip_open(chip_path);
+}
+
+static int helper_get_line(enum adapter_gpio_config_index idx)
+{
+	struct gpiod_request_config *req_cfg = NULL;
+	int retval = -1;
+
+	if (!is_gpio_config_valid(idx))
+		return ERROR_OK;
+
+	gpiod_chip[idx] = gpiod_chip_open_by_number(adapter_gpio_config[idx].chip_num);
+	if (!gpiod_chip[idx]) {
+		LOG_ERROR("Cannot open LinuxGPIOD chip %d for %s", adapter_gpio_config[idx].chip_num,
+			adapter_gpio_get_name(idx));
+		return ERROR_JTAG_INIT_FAILED;
+	}
+
+	gpiod_line_settings[idx] = gpiod_line_settings_new();
+	gpiod_line_config[idx] = gpiod_line_config_new();
+	req_cfg = gpiod_request_config_new();
+
+	if (!gpiod_line_settings[idx] || !gpiod_line_config[idx] || !req_cfg) {
+		LOG_ERROR("Cannot configure LinuxGPIOD line for %s", adapter_gpio_get_name(idx));
+		return ERROR_JTAG_INIT_FAILED;
+	}
+
+	gpiod_request_config_set_consumer(req_cfg, "OpenOCD");
+
+	switch (adapter_gpio_config[idx].init_state) {
+	case ADAPTER_GPIO_INIT_STATE_INPUT:
+		gpiod_line_settings_set_direction(gpiod_line_settings[idx], GPIOD_LINE_DIRECTION_INPUT);
+		break;
+	case ADAPTER_GPIO_INIT_STATE_INACTIVE:
+		gpiod_line_settings_set_direction(gpiod_line_settings[idx], GPIOD_LINE_DIRECTION_OUTPUT);
+		gpiod_line_settings_set_output_value(gpiod_line_settings[idx], GPIOD_LINE_VALUE_INACTIVE);
+		break;
+	case ADAPTER_GPIO_INIT_STATE_ACTIVE:
+		gpiod_line_settings_set_direction(gpiod_line_settings[idx], GPIOD_LINE_DIRECTION_OUTPUT);
+		gpiod_line_settings_set_output_value(gpiod_line_settings[idx], GPIOD_LINE_VALUE_ACTIVE);
+		break;
+	}
+
+	if (adapter_gpio_config[idx].init_state != ADAPTER_GPIO_INIT_STATE_INPUT) {
+		switch (adapter_gpio_config[idx].drive) {
+		case ADAPTER_GPIO_DRIVE_MODE_PUSH_PULL:
+			gpiod_line_settings_set_drive(gpiod_line_settings[idx], GPIOD_LINE_DRIVE_PUSH_PULL);
+			break;
+		case ADAPTER_GPIO_DRIVE_MODE_OPEN_DRAIN:
+			gpiod_line_settings_set_drive(gpiod_line_settings[idx], GPIOD_LINE_DRIVE_OPEN_DRAIN);
+			break;
+		case ADAPTER_GPIO_DRIVE_MODE_OPEN_SOURCE:
+			gpiod_line_settings_set_drive(gpiod_line_settings[idx], GPIOD_LINE_DRIVE_OPEN_SOURCE);
+			break;
+		}
+	}
+
+	switch (adapter_gpio_config[idx].pull) {
+	case ADAPTER_GPIO_PULL_NONE:
+#ifdef GPIOD_LINE_BIAS_DISABLED
+		gpiod_line_settings_set_bias(gpiod_line_settings[idx], GPIOD_LINE_BIAS_DISABLED);
+#endif
+		break;
+	case ADAPTER_GPIO_PULL_UP:
+#ifdef GPIOD_LINE_BIAS_PULL_UP
+		gpiod_line_settings_set_bias(gpiod_line_settings[idx], GPIOD_LINE_BIAS_PULL_UP);
+#else
+		LOG_WARNING("linuxgpiodv2: ignoring request for pull-up on %s: not supported by gpiod v%s",
+			adapter_gpio_get_name(idx), gpiod_api_version());
+#endif
+		break;
+	case ADAPTER_GPIO_PULL_DOWN:
+#ifdef GPIOD_LINE_BIAS_PULL_DOWN
+		gpiod_line_settings_set_bias(gpiod_line_settings[idx], GPIOD_LINE_BIAS_PULL_DOWN);
+#else
+		LOG_WARNING("linuxgpiodv2: ignoring request for pull-down on %s: not supported by gpiod v%s",
+			adapter_gpio_get_name(idx), gpiod_api_version());
+#endif
+		break;
+	}
+
+	gpiod_line_settings_set_active_low(gpiod_line_settings[idx], adapter_gpio_config[idx].active_low);
+
+	retval = gpiod_line_config_add_line_settings(gpiod_line_config[idx],
+			&adapter_gpio_config[idx].gpio_num, 1, gpiod_line_settings[idx]);
+	if (retval < 0) {
+		gpiod_request_config_free(req_cfg);
+		LOG_ERROR("Error configuring gpio line %s", adapter_gpio_get_name(idx));
+		return ERROR_JTAG_INIT_FAILED;
+	}
+
+	gpiod_line_request[idx] = gpiod_chip_request_lines(gpiod_chip[idx], req_cfg, gpiod_line_config[idx]);
+	gpiod_request_config_free(req_cfg);
+
+	if (!gpiod_line_request[idx]) {
+		LOG_ERROR("Error requesting gpio line %s", adapter_gpio_get_name(idx));
+		return ERROR_JTAG_INIT_FAILED;
+	}
+
+	return ERROR_OK;
+}
+
+static int linuxgpiod_init(void)
+{
+	LOG_INFO("Linux GPIOD JTAG/SWD bitbang driver (libgpiod v2)");
+
+	bitbang_interface = &linuxgpiod_bitbang;
+	adapter_gpio_config = adapter_gpio_get_config();
+
+	/*
+	 * Configure JTAG/SWD signals. Default directions and initial states are handled
+	 * by adapter.c and "adapter gpio" command.
+	 */
+
+	if (transport_is_jtag()) {
+		if (!linuxgpiod_jtag_mode_possible()) {
+			LOG_ERROR("Require tck, tms, tdi and tdo gpios for JTAG mode");
+			goto out_error;
+		}
+
+		if (helper_get_line(ADAPTER_GPIO_IDX_TDO) != ERROR_OK ||
+			helper_get_line(ADAPTER_GPIO_IDX_TDI) != ERROR_OK ||
+			helper_get_line(ADAPTER_GPIO_IDX_TCK) != ERROR_OK ||
+			helper_get_line(ADAPTER_GPIO_IDX_TMS) != ERROR_OK ||
+			helper_get_line(ADAPTER_GPIO_IDX_TRST) != ERROR_OK)
+				goto out_error;
+	}
+
+	if (transport_is_swd()) {
+		int retval1, retval2;
+		if (!linuxgpiod_swd_mode_possible()) {
+			LOG_ERROR("Require swclk and swdio gpio for SWD mode");
+			goto out_error;
+		}
+
+		/*
+		 * swdio and its buffer should be initialized in the order that prevents
+		 * two outputs from being connected together. This will occur if the
+		 * swdio GPIO is configured as an output while the external buffer is
+		 * configured to send the swdio signal from the target to the GPIO.
+		 */
+		if (adapter_gpio_config[ADAPTER_GPIO_IDX_SWDIO].init_state == ADAPTER_GPIO_INIT_STATE_INPUT) {
+			retval1 = helper_get_line(ADAPTER_GPIO_IDX_SWDIO);
+			retval2 = helper_get_line(ADAPTER_GPIO_IDX_SWDIO_DIR);
+		} else {
+			retval1 = helper_get_line(ADAPTER_GPIO_IDX_SWDIO_DIR);
+			retval2 = helper_get_line(ADAPTER_GPIO_IDX_SWDIO);
+		}
+		if (retval1 != ERROR_OK || retval2 != ERROR_OK)
+			goto out_error;
+
+		if (helper_get_line(ADAPTER_GPIO_IDX_SWCLK) != ERROR_OK)
+			goto out_error;
+	}
+
+	if (helper_get_line(ADAPTER_GPIO_IDX_SRST) != ERROR_OK ||
+		helper_get_line(ADAPTER_GPIO_IDX_LED) != ERROR_OK)
+			goto out_error;
+
+	return ERROR_OK;
+
+out_error:
+	linuxgpiod_quit();
+
+	return ERROR_JTAG_INIT_FAILED;
+}
+
+static const char *const linuxgpiod_transport[] = { "swd", "jtag", NULL };
+
+static struct jtag_interface linuxgpiod_interface = {
+	.supported = DEBUG_CAP_TMS_SEQ,
+	.execute_queue = bitbang_execute_queue,
+};
+
+struct adapter_driver linuxgpiod_adapter_driver = {
+	.name = "linuxgpiod",
+	.transports = linuxgpiod_transport,
+
+	.init = linuxgpiod_init,
+	.quit = linuxgpiod_quit,
+	.reset = linuxgpiod_reset,
+
+	.jtag_ops = &linuxgpiod_interface,
+	.swd_ops = &bitbang_swd,
+};
